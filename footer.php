863b7d7fa261d67e4a85b2a31611384b285b246d
Robb Lewis <me@therobb.com>
2015-03-28 17:39:23 +0000

    Add instantclick

--- footer.php
+++ footer.php
@@ -13,6 +13,9 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 		<script src="<?php echo theme_url('/js/magnific.min.js'); ?>"></script>
 
+		<script src="instantclick.min.js" data-no-instant></script>
+		<script data-no-instant>InstantClick.init();</script>
+
 		<script type="text/javascript">
 			$(document).ready(function() {
 				$('.article__content').magnificPopup({
