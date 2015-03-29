57c18f421f493618b5fedb74979604567464dfa1
Robb Lewis <me@therobb.com>
2015-03-29 20:08:42 +0100

    Update 404

--- 404.php
+++ 404.php
@@ -3,7 +3,8 @@
 	<section class="content wrap">
 		<h1>Page not found</h1>
 
-		<p>Unfortunately, the page <code>/<?php echo current_url(); ?></code> could not be found. Your best bet is either to try the <a href="<?php echo base_url(); ?>">homepage</a>, try <a href="#search">searching</a>, or go and cry in a corner (although I don’t recommend the latter).</p>
+		<p>Unfortunately, the page could not be found.</p>
+		<p>Try the <a href="<?php echo base_url(); ?>">posts page</a> or the <a href="/browse">browse page</a> for what you're looking for.</p>
 	</section>
 
 <?php theme_include('footer'); ?>
