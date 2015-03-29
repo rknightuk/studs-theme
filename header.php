972345f7bc0b0974dfd777b9c1dce9f636600c17
Robb Lewis <me@therobb.com>
2015-03-29 10:26:37 +0100

    Move gs

--- header.php
+++ header.php
@@ -18,6 +18,15 @@
 		<link rel="stylesheet" href="<?php echo theme_url('/css/magnific.css'); ?>">
 		
 		<link rel="alternate" type="application/rss+xml" title="RSS" href="<?php echo rss_url(); ?>">
+
+		<script>
+		  !function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
+		  arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
+		  d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
+		  insertBefore(d,q)}(window,document,'script','_gs');
+
+		  _gs('GSN-973214-C');
+		</script>
 		
 	</head>
 	
